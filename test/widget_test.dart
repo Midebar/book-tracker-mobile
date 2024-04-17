b6339e99fda2280a2bf937bc989f984e6125609a
Mikhael <midebar@gmail.com>
2024-04-17 23:32:02 +0700

    Finished Tutorial 7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:book_tracker/menu.dart';
+import 'package:book_tracker/screens/menu.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
